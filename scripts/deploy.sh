77139d67be2c194be669252d6784d25d99157908
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2024-09-17 02:05:55 -0500

    Add terraform init to deploy script
    Signed-off-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,5 +10,6 @@ cd - || exit 1
 GOOS=linux GOARCH=amd64 go build -o ./bin/api
 docker buildx build --platform="linux/amd64" --push -t "lucasrod96/oss-contribute:${IMAGE_VERSION}" .
 
+terraform init
 terraform plan -var="image_version=${IMAGE_VERSION}"
 terraform apply --auto-approve -var="image_version=${IMAGE_VERSION}"
