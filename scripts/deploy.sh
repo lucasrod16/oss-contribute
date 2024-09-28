77e56327eeb7d495c48187615fc00c380554de28
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2024-09-28 16:54:24 -0500

    Get correct manifest digest
    Signed-off-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/api
 echo "${DOCKER_PASSWORD}" | docker login --username="${DOCKER_USERNAME}" --password-stdin
 docker buildx build --no-cache --platform="linux/amd64" --push -t "lucasrod96/oss-contribute:${IMAGE_VERSION}" .
 
-IMAGE_DIGEST=$(docker buildx imagetools inspect --raw "lucasrod96/oss-contribute:${IMAGE_VERSION}" | jq -r '.config.digest')
+IMAGE_DIGEST=$(docker buildx imagetools inspect "lucasrod96/oss-contribute:${IMAGE_VERSION}" --format "{{json .Manifest}}" | jq -r '.digest')
 
 terraform init
 terraform plan -var="image_digest=${IMAGE_DIGEST}"
