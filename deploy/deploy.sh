dfcf46cf1fc475e12f56821502370b7565be4434
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2025-08-03 01:29:51 -0500

    use array

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 server_ip=$(terraform output -raw server_ip)
-ssh_opts="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+ssh_opts=(-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null)
 
-ssh "${ssh_opts}" root@"${server_ip}" -- mkdir -p /root/oss-projects
-scp "${ssh_opts}" docker-compose.yml root@"${server_ip}":/root/oss-projects/docker-compose.yml
-scp "${ssh_opts}" -r conf root@"${server_ip}":/root/oss-projects/
-ssh "${ssh_opts}" root@"${server_ip}" -- docker compose -f /root/oss-projects/docker-compose.yml up -d
+ssh "${ssh_opts[@]}" root@"${server_ip}" -- mkdir -p /root/oss-projects
+scp "${ssh_opts[@]}" docker-compose.yml root@"${server_ip}":/root/oss-projects/docker-compose.yml
+scp "${ssh_opts[@]}" -r conf root@"${server_ip}":/root/oss-projects/
+ssh "${ssh_opts[@]}" root@"${server_ip}" -- docker compose -f /root/oss-projects/docker-compose.yml up -d
