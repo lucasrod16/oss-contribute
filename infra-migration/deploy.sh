25550ddb4b21207dfc9a5f917706925ba568629f
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2025-08-02 22:13:26 -0500

    use caddy for https

--- infra-migration/deploy.sh
+++ infra-migration/deploy.sh
@@ -6,4 +6,5 @@ server_ip=$(terraform output -raw server_ip)
 
 ssh root@"${server_ip}" -- mkdir -p /root/oss-projects
 scp docker-compose.yml root@"${server_ip}":/root/oss-projects/docker-compose.yml
+scp -r conf root@"${server_ip}":/root/oss-projects/
 ssh root@"${server_ip}" -- docker compose -f /root/oss-projects/docker-compose.yml up -d
