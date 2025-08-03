58d1deb872b9719cb11df29e752d869dd2ff0866
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2025-08-03 01:54:12 -0500

    v0.1.4 build

--- deploy/build.sh
+++ deploy/build.sh
@@ -10,8 +10,8 @@ npm --prefix="${UI_DIR}" run build
 
 pushd "${REPO_ROOT}"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./bin/api
-docker buildx build --platform="linux/amd64" --push -t "lucasrod96/oss-projects-hetzner-migration:latest" .
+docker buildx build --platform="linux/amd64" --push -t "lucasrod96/oss-projects:${IMAGE_TAG}" .
 popd
 
-IMAGE_DIGEST=$(docker buildx imagetools inspect "lucasrod96/oss-projects-hetzner-migration:latest" --format "{{json .Manifest}}" | jq -r '.digest')
-yq -i ".services.oss-projects.image = \"lucasrod96/oss-projects-hetzner-migration@${IMAGE_DIGEST}\"" docker-compose.yml
+IMAGE_DIGEST=$(docker buildx imagetools inspect "lucasrod96/oss-projects:${IMAGE_TAG}" --format "{{json .Manifest}}" | jq -r '.digest')
+yq -i ".services.oss-projects.image = \"lucasrod96/oss-projects:${IMAGE_TAG}@${IMAGE_DIGEST}\"" docker-compose.yml
